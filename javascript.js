317dbe2e20c27f05e8a46c6870ae33c9e6369744
Zak1012-a <156977956+Zak1012-a@users.noreply.github.com>
2024-03-27 09:40:44 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,612 @@
-var e = document.getElementById("test");
-			function onChange() {
-  			var value = e.value;
-  			var text = e.options[e.selectedIndex].text;
-  			console.log(value, text);
-		       }
-		       e.onchange = onChange;
-		       onChange();
+
+var e = document.getElementById("test1");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("test2");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("test3");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
+
+var e = document.getElementById("");
+function onChange() {
+  	var value = e.value;
+  	var text = e.options[e.selectedIndex].text;
+  	console.log(value, text);
+	}
+e.onchange = onChange;
+onChange();
